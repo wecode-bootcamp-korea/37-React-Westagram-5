6d379bdf2393aad257aaa3333de3049404b78b43
Hoon <si932174@gmail.com>
2022-08-31 11:31:52 +0900

    directory 정리

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
+import Router from './Router';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './styles/reset.scss';
+import './styles/common.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    
+    <Router />
   </React.StrictMode>
 );
