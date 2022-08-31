823989b49c9a89b833993434d34a6f280f7cfc05
annara <annari1017@gmail.com>
2022-08-31 17:18:33 +0900

    Add: 로그인페이지, 메인페이지 추가 - js파일, scss파일, 이미지파일 추가

--- src/pages/naraAn/Login/Login.js
+++ src/pages/naraAn/Login/Login.js
@@ -1,7 +1,44 @@
 import React from 'react';
+import './Login.scss';
+import { useNavigate } from 'react-router-dom';
 
-function LoginNara() {
-  return <h1>naraLogin</h1>;
-}
+const Login = () => {
+  const navigate = useNavigate();
 
-export default LoginNara;
+  const goToMain = () => {
+    navigate('/main-nara');
+  };
+
+  return (
+    <section className="login_main">
+      <form className="login_form">
+        <h1 className="title">Westagram</h1>
+        <div className="login_input">
+          <input
+            className="input_style"
+            id="id"
+            type="password"
+            placeholder="전화번호, 사용자 이름 또는 이메일"
+          />
+          <input
+            className="input_style"
+            id="password"
+            type="password"
+            placeholder="비밀번호"
+          />
+          <input
+            className="login_btn"
+            onClick={goToMain}
+            type="submit"
+            value="로그인"
+          />
+        </div>
+        <div className="find_password">
+          <button>비밀번호를 잊으셨나요?</button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default Login;
--- src/pages/naraAn/Main/Main.js
+++ src/pages/naraAn/Main/Main.js
@@ -1,7 +1,236 @@
 import React from 'react';
+import './Main.scss';
 
-function MainNara() {
-  return <h1>naraMain</h1>;
+function Main() {
+  return (
+    <>
+      <nav className="nav">
+        <div className="nav_logoAndTitle">
+          <i className="fa-brands fa-instagram" />
+          <span className="title">Westagram</span>
+        </div>
+        <form className="nav_search">
+          <input type="text" className="input_style" />
+          <i className="fa-solid fa-magnifying-glass search-icon"> 검색</i>
+        </form>
+        <ul className="nav_icons">
+          <li>
+            <button>
+              <i className="fa-regular fa-compass" />
+            </button>
+          </li>
+          <li>
+            <button>
+              <i className="fa-regular fa-heart" />
+            </button>
+          </li>
+          <li>
+            <button>
+              <i className="fa-regular fa-user" />
+            </button>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <div className="feeds">
+          <article>
+            <header className="feeds_header">
+              <div className="user_data">
+                <img
+                  className="user_img"
+                  alt="character"
+                  src="../../../../images/naraAn/tomung.jpeg"
+                />
+                <span className="user_name">nariiiii</span>
+              </div>
+              <button>
+                <i className="fa-solid fa-ellipsis" />
+              </button>
+            </header>
+            <div className="feeds_main">
+              <span className="main_img">
+                <img
+                  alt="waikiki beach"
+                  src="../../../../images/naraAn/waikiki.jpeg"
+                />
+              </span>
+              <div className="feed_contents">
+                <ul className="feed_icons">
+                  <li>
+                    <button>
+                      <i className="fa-regular fa-heart" />
+                    </button>
+                    <button>
+                      <i className="fa-regular fa-comment" />
+                    </button>
+                    <button>
+                      <i className="fa-regular fa-paper-plane" />
+                    </button>
+                  </li>
+                  <li>
+                    <button>
+                      <i className="fa-regular fa-bookmark" />
+                    </button>
+                  </li>
+                </ul>
+                <div className="feed_likes">
+                  <img
+                    className="user_img"
+                    alt="minions"
+                    src="../../../../images/naraAn/minions.jpg"
+                  />
+                  <span className="user_name">minions</span>
+                  <span>님 외 10명이 좋아합니다</span>
+                </div>
+                <ul className="feed_text">
+                  <li>
+                    <span className="user_name">nariiiii</span>
+                    <span className="comment_style">여행가고 싶다 🥲</span>
+                  </li>
+                  <li>
+                    <button>더보기</button>
+                  </li>
+                </ul>
+                <div id="new_comment_location" />
+                <p>42분 전</p>
+              </div>
+            </div>
+            <footer>
+              <form id="comment_write">
+                <input
+                  type="text"
+                  className="comment_input"
+                  placeholder="댓글달기..."
+                />
+                <input id="comment_btn" type="submit" value="게시" />
+              </form>
+            </footer>
+          </article>
+        </div>
+        <div className="main_right">
+          <header className="main_right_title">
+            <img
+              className="user_img"
+              alt="duck"
+              src="../../../../images/naraAn/duck.jpg"
+            />
+            <ul>
+              <li id="user_wecode" className="user_name">
+                wecode_37th
+              </li>
+              <li>
+                <p>wecode | 위코드</p>
+              </li>
+            </ul>
+          </header>
+          <div className="main_right_story">
+            <div className="story_title">
+              <p>스토리</p>
+              <button>모두보기</button>
+            </div>
+            <div className="story_user">
+              <img
+                alt="minions"
+                src="../../../../images/naraAn/minions.jpg"
+                className="user_img"
+              />
+              <div className="user_data">
+                <span className="user_name">minions</span>
+                <p className="time">10분 전</p>
+              </div>
+            </div>
+            <div className="story_user">
+              <img
+                alt="minions"
+                src="../../../../images/naraAn/minions.jpg"
+                className="user_img"
+              />
+              <div className="user_data">
+                <span className="user_name">minions</span>
+                <p className="time">10분 전</p>
+              </div>
+            </div>
+            <div className="story_user">
+              <img
+                alt="minions"
+                src="../../../../images/naraAn/minions.jpg"
+                className="user_img"
+              />
+              <div className="user_data">
+                <span className="user_name">minions</span>
+                <p className="time">10분 전</p>
+              </div>
+            </div>
+            <div className="story_user">
+              <img
+                alt="minions"
+                src="../../../../images/naraAn/minions.jpg"
+                className="user_img"
+              />
+              <div className="user_data">
+                <span className="user_name">minions</span>
+                <p className="time">10분 전</p>
+              </div>
+            </div>
+          </div>
+          <div className="main_right_recommend">
+            <div className="recommend_title">
+              <p>회원님을 위한 추천</p>
+              <button>모두보기</button>
+            </div>
+            <ul className="recommend_user">
+              <li>
+                <img
+                  alt="minions"
+                  src="../../../../images/naraAn/minions.jpg"
+                  className="user_img"
+                />
+              </li>
+              <li className="user_data">
+                <span className="user_name">minions</span>
+                <p>_juhyunii님 외 2명이...</p>
+              </li>
+              <li>
+                <button>팔로우</button>
+              </li>
+            </ul>
+            <ul className="recommend_user">
+              <li>
+                <img
+                  alt="minions"
+                  src="../../../../images/naraAn/minions.jpg"
+                  className="user_img"
+                />
+              </li>
+              <li className="user_data">
+                <span className="user_name">minions</span>
+                <p>_juhyunii님 외 2명이...</p>
+              </li>
+              <li>
+                <button>팔로우</button>
+              </li>
+            </ul>
+            <ul className="recommend_user">
+              <li>
+                <img
+                  alt="minions"
+                  src="../../../../images/naraAn/minions.jpg"
+                  className="user_img"
+                />
+              </li>
+              <li className="user_data">
+                <span className="user_name">minions</span>
+                <p>_juhyunii님 외 2명이...</p>
+              </li>
+              <li>
+                <button>팔로우</button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </main>
+    </>
+  );
 }
 
-export default MainNara;
+export default Main;
