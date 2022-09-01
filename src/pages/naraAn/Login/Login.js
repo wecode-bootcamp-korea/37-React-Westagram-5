8291ad9bb9f2986c087fe791a33b25e5e8e86f50
annara <annari1017@gmail.com>
2022-09-01 15:01:52 +0900

    Add:Mission 1-사용자 입력 데이터 저장 기능 구현

--- src/pages/naraAn/Login/Login.js
+++ src/pages/naraAn/Login/Login.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Login.scss';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 const Login = () => {
   const navigate = useNavigate();
@@ -9,6 +10,15 @@ const Login = () => {
     navigate('/main-nara');
   };
 
+  const [id, userId] = useState('');
+  function saveUserId(e) {
+    userId(e.target.value);
+  }
+
+  const [pw, userPw] = useState('');
+  function saveUserPw(e) {
+    userPw(e.target.value);
+  }
   return (
     <section className="login_main">
       <form className="login_form">
@@ -16,12 +26,14 @@ const Login = () => {
         <div className="login_input">
           <input
             className="input_style"
+            onChange={saveUserId}
             id="id"
             type="password"
             placeholder="전화번호, 사용자 이름 또는 이메일"
           />
           <input
             className="input_style"
+            onChange={saveUserPw}
             id="password"
             type="password"
             placeholder="비밀번호"
